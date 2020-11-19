91e042a6c995181a213dadd5ec78ea6449bdbb9f
Daniela Perez <durodaniela@gmail.com>
2020-11-19 17:25:30 +0100

    modified

--- pract_4.sh
+++ pract_4.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-cat -n Data/shell/Text_example.txt > Text_example_new.txt
+cat -n ~/Data/shell/Text_example.txt > Text_example_new.txt
+
+cat -n ~/Data/shell/Text_example.txt 
 
 echo "ciao"
