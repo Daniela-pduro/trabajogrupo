d3fe11496ad561f89de5e71abef139d5571c2bfc
Daniela Perez <durodaniela@gmail.com>
2020-11-19 18:12:03 +0100

    modified

---
+++ pract_5.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo '#!/Bin/Bash'
+
+echo This file is created by $USER on $(date)
+
+echo '#!/Bin/Bash' > new_file ; echo This file is created by $USER on $(date) >> new_file
+
+echo "ciao"
